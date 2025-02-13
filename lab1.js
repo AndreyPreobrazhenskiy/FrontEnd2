3b2fd3347dc8b91db627c1870b5d099e0dc8b9f4
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2025-02-13 19:40:15 +0400

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,24 @@
+//Вариант 3
+//2
+const sumNumbers = (num1, num2, num3) => {
+    return num1 + num2 + num3;
+};
+//3
+let isStudent = true;
+const age = 19;
+const surname = "Preobrazhenskiy";
+let info = {
+    isStudent: true,
+    age: 20,
+    surname: "Soprano",
+    arr: [1, 2, 3, 4, 5]
+};
+const binarySet = [true, false, true, true];
+let point = [5, 8];
+const data = {
+    id: 3,
+    e1: 12345,
+    e2: "Поле 2",
+};
+const jsonData = JSON.stringify(data);
+console.log(jsonData);
