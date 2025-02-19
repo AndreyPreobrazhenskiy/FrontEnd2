9ad3b2269ff7919abbcc6a2cd1c086e51715f513
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2025-02-19 19:59:33 +0400

    Add files via upload

---
+++ lab5.ts
@@ -0,0 +1,29 @@
+//Вариант 3
+
+const data: string[] = [];
+
+const record = (param: string) => {
+    data.push(param);
+    console.log('Record: ' + param + ' added!');
+    
+    setTimeout(() => {
+        const index = data.indexOf(param);
+        if (index !== -1) {
+            data.splice(index, 1);
+            console.log('Record: ' + param + ' removed!');
+        }
+    }, 10000);
+};
+
+// Функция, возвращающая функцию
+const createLogger = (message: string) => {
+    return () => console.log(message);
+};
+
+// Вызов метода record
+record("example1");
+record("example2");
+
+// Использование замыкания
+const logHello = createLogger("Hello, closures!");
+logHello();
